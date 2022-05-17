096af5daa3f872fd3277d20c6c379b00d8e30d2f
Victor Nunes <victor.nunes@arquivei.com.br>
2022-05-17 09:29:37 -0300

    Adds parallel processor core (#2)
    * Adds parallel processor core
    
    Adds the Parallel Processor interface and the parallel processor
    implementation.
    
    Adds a Option to subscribe the Deduplicate Documents function
    
    Adds unit tests

--- go.mod
+++ go.mod
@@ -1,3 +1,43 @@
 module github.com/gomsgprocessor
 
 go 1.18
+
+require (
+	github.com/arquivei/foundationkit v0.1.2
+	github.com/rs/zerolog v1.26.1
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29
+)
+
+require (
+	cloud.google.com/go v0.97.0 // indirect
+	cloud.google.com/go/monitoring v1.1.0 // indirect
+	cloud.google.com/go/trace v1.0.0 // indirect
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.10 // indirect
+	github.com/aws/aws-sdk-go v1.42.6 // indirect
+	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cncf/udpa/go v0.0.0-20210930031921-04548b0d99d4 // indirect
+	github.com/cncf/xds/go v0.0.0-20211011173535-cb28da3451f1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/envoyproxy/go-control-plane v0.10.1 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.6.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20211116231205-47ca1ff31462 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20211116061358-0a5406a5449c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.60.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20211117155847-120650a500bb // indirect
+	google.golang.org/grpc v1.42.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- parallerprocessor.go
+++ parallerprocessor.go
@@ -1,1 +1,150 @@
 package gomsgprocessor
+
+import (
+	"context"
+
+	"github.com/arquivei/foundationkit/errors"
+	"github.com/arquivei/foundationkit/trace"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/sync/errgroup"
+)
+
+// ParallelProcessor is a interface that process in parallel a slice of Message.
+type ParallelProcessor interface {
+	MakeDocuments(context.Context, []Message) ([]Document, error)
+}
+
+// DocumentBuilder is a interface that transforms a Message into []Document.
+type DocumentBuilder interface {
+	Build(context.Context, Message) ([]Document, error)
+}
+
+type parallelProcessor struct {
+	builders             map[MessageType]DocumentBuilder
+	deduplicateDocuments DeduplicateDocumentsFunc
+}
+
+// NewParallelProcessor returns a new ParallelProcessor with a map of
+// DocumentBuilder for each MessageType.
+//
+// A list of Option is also available for this method. See option.go for more
+// information.
+func NewParallelProcessor(
+	builders map[MessageType]DocumentBuilder,
+	opts ...Option,
+) ParallelProcessor {
+	p := &parallelProcessor{
+		builders:             builders,
+		deduplicateDocuments: defaultDeduplicateDocumentsFunc,
+	}
+
+	for _, opt := range opts {
+		opt(p)
+	}
+
+	return p
+}
+
+// MakeDocuments creates in parallel a slice of Document for given []Message
+// using the map of DocumentBuilder (see NewParallelProcessor).
+//
+// This method returns a []Document and a (foundationkit/errors).Error.
+// If not nil, this error has a (foundationkit/errors).Code associated with and
+// can be a ErrCodeBuildDocuments or a ErrCodeDeduplicateDocuments.
+func (p *parallelProcessor) MakeDocuments(ctx context.Context, msgs []Message) ([]Document, error) {
+	const op = errors.Op("gomsgprocessor.parallelProcessor.MakeDocuments")
+
+	ctx, span := trace.StartSpan(ctx, op.String())
+	defer span.End(nil)
+
+	documentsByNamespace, err := p.parallelBuildDocumentsByNamespace(ctx, msgs)
+	if err != nil {
+		return nil, errors.E(op, err, ErrCodeBuildDocuments)
+	}
+
+	deduplicatedDocuments, err := p.deduplicateDocumentsForEachNamespace(documentsByNamespace)
+	if err != nil {
+		return nil, errors.E(op, err, ErrCodeDeduplicateDocuments)
+	}
+
+	return deduplicatedDocuments, nil
+}
+
+func (p *parallelProcessor) parallelBuildDocumentsByNamespace(
+	ctx context.Context,
+	msgs []Message,
+) (map[Namespace][]Document, error) {
+	const op = errors.Op("parallelBuildDocumentsByNamespace")
+
+	type builtDocument struct {
+		documents []Document
+		namespace Namespace
+	}
+
+	builtDocuments := make([]builtDocument, len(msgs))
+
+	g, ctx := errgroup.WithContext(ctx)
+	for i, msg := range msgs {
+		i, msg := i, msg
+
+		g.Go(func() error {
+			documentBuilder, ok := p.builders[msg.GetType()]
+			if !ok {
+				msg.UpdateLogWithData(ctx)
+				return errors.E(ErrMsgTypeHasNoBuilder, errors.KV("type", msg.GetType()))
+			}
+
+			documents, err := documentBuilder.Build(ctx, msg)
+			if err != nil {
+				msg.UpdateLogWithData(ctx)
+				return err
+			}
+
+			if documents == nil {
+				msg.UpdateLogWithData(ctx)
+				log.Ctx(ctx).Info().Msg("Message ignored...")
+				return nil
+			}
+
+			builtDocuments[i] = builtDocument{
+				documents: documents,
+				namespace: msg.GetNamespace(),
+			}
+
+			return nil
+		})
+	}
+
+	err := g.Wait()
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+
+	documentsByNamespace := make(map[Namespace][]Document, len(builtDocuments))
+	for _, builtDoc := range builtDocuments {
+		if builtDoc.documents == nil || builtDoc.namespace == "" {
+			continue
+		}
+		documentsByNamespace[builtDoc.namespace] = append(
+			documentsByNamespace[builtDoc.namespace],
+			builtDoc.documents...,
+		)
+	}
+	return documentsByNamespace, nil
+}
+
+func (p *parallelProcessor) deduplicateDocumentsForEachNamespace(
+	documentsByNamespace map[Namespace][]Document,
+) ([]Document, error) {
+	const op = errors.Op("deduplicateDocumentsForEachNamespace")
+
+	documents := make([]Document, 0, len(documentsByNamespace))
+	for _, docs := range documentsByNamespace {
+		deduplicatedDocuments, err := p.deduplicateDocuments(docs)
+		if err != nil {
+			return nil, errors.E(op, err)
+		}
+		documents = append(documents, deduplicatedDocuments...)
+	}
+	return documents, nil
+}
